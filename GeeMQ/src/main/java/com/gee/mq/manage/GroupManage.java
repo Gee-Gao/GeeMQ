6e918d28abb10c8ae118fb8edcfec29aeb8e73f2
Gee <1176243316@qq.com>
2022-02-09 11:52:31 +0800

    重构消费者分组

--- GeeMQ/src/main/java/com/gee/mq/manage/GroupManage.java
+++ GeeMQ/src/main/java/com/gee/mq/manage/GroupManage.java
@@ -2,12 +2,14 @@ package com.gee.mq.manage;
 
 import com.gee.mq.bean.Group;
 import com.gee.mq.consumer.QueueConsumer;
+import com.gee.mq.consumer.TopicConsumer;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,23 +24,33 @@ public class GroupManage {
     private volatile ConcurrentHashMap<String, Group> groupHashMap = new ConcurrentHashMap<>(1024);
 
     private final QueueConsumer queueConsumer;
+
+    private final TopicConsumer topicConsumer;
+
     // 消费者分组
     @GetMapping("queue")
     public void groupConsumer(String groupName, Set<String> consumerNames) {
-        List<String> consumerThreadNameList = queueConsumer.consumerThreadName();
+        List<String> queueConsumerNameList = queueConsumer.consumerThreadName();
+        List<String> topicConsumerNameList = topicConsumer.consumerThreadName();
+
+        List<String> consumerNameList = new ArrayList<>(queueConsumerNameList.size() + topicConsumerNameList.size());
+
+        consumerNameList.addAll(queueConsumerNameList);
+        consumerNameList.addAll(topicConsumerNameList);
+
         Group group = groupHashMap.get(groupName);
         if (group == null) {
-            group=new Group();
+            group = new Group();
             group.setGroupName(groupName);
             Set<String> collect = consumerNames.stream()
-                    .filter(consumerThreadNameList::contains)
+                    .filter(consumerNameList::contains)
                     .collect(Collectors.toSet());
             group.setConsumers(collect);
-            groupHashMap.put(groupName,group);
-        }else {
+            groupHashMap.put(groupName, group);
+        } else {
             Set<String> consumers = group.getConsumers();
             Set<String> collect = consumerNames.stream()
-                    .filter(consumerThreadNameList::contains)
+                    .filter(consumerNameList::contains)
                     .collect(Collectors.toSet());
             consumers.addAll(collect);
         }
