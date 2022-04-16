e7c13978a07e772ea660a0b8ed605ea82db02072
Gee <1176243316@qq.com>
2022-04-16 12:17:54 +0800

    1.登录优化 2.注册用户

--- GeeMQ/src/main/java/com/gee/mq/manage/UserManage.java
+++ GeeMQ/src/main/java/com/gee/mq/manage/UserManage.java
@@ -21,7 +21,7 @@ public class UserManage {
     static List<User> allUsers;
 
     static {
-        allUsers=new ArrayList<>();
+        allUsers = new ArrayList<>();
 
         User admin = new User();
         admin.setUsername("admin");
@@ -50,11 +50,32 @@ public class UserManage {
     }
 
     @PostMapping("login")
-    private Result login(User user) {
-        if ("admin".equals(user.getUsername()) && "123456".equals(user.getPassword())) {
+    public Result login(User user) {
+        long count = allUsers.stream().filter(item -> item.equals(user)).count();
+        if (count > 0) {
             return Result.ok();
         } else {
             throw new RuntimeException("账号或密码错误");
         }
+    }
+
+    @PostMapping("register")
+    public Result register(User user) {
+        if (user.getUsername() == null) {
+            throw new RuntimeException("账号不能为空");
+        }
+
+        if (user.getPassword() == null) {
+            throw new RuntimeException("密码不能为空");
+        }
+
+        long count = allUsers.stream().filter(item -> item.equals(user)).count();
+
+        if (count > 0) {
+            throw new RuntimeException("用户已存在");
+        }
+
+        allUsers.add(user);
+        return Result.ok();
     }
 }
