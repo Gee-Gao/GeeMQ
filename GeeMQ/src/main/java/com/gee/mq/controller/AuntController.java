196bdb90605e519132a1943040a47fd722bd2889
Gee <1176243316@qq.com>
2022-09-05 22:00:06 +0800

    姨妈分析优化

--- GeeMQ/src/main/java/com/gee/mq/controller/AuntController.java
+++ GeeMQ/src/main/java/com/gee/mq/controller/AuntController.java
@@ -8,14 +8,8 @@ import com.gee.mq.service.AuntService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -39,84 +33,15 @@ public class AuntController {
         return Result.ok();
     }
 
-    @PostMapping("/analyzer")
-    public Result analyzer(@RequestBody User user) {
-        List<LocalDate> list = getAuntLocalDate(user);
-        Map<String, Object> result = calcAunt(list);
-        return Result.ok(result);
+    @PostMapping("/auntInterval")
+    public Result analyzerAuntInterval(@RequestBody User user) {
+        List<LocalDate> list = auntService.getAuntLocalDate(user);
+        return Result.ok(auntService.auntInterval(list));
     }
 
-    private List<LocalDate> getAuntLocalDate(User user) {
-        List<Aunt> auntList = auntService.list(new LambdaQueryWrapper<Aunt>()
-                .eq(Aunt::getUserId, user.getId())
-                .orderByAsc(Aunt::getAuntDate));
-        return auntList.stream().map(item -> {
-            Date auntDate = item.getAuntDate();
-            return auntDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        }).collect(Collectors.toList());
+    @PostMapping("/auntRecord")
+    public Result analyzerAuntRecord(@RequestBody User user) {
+        List<LocalDate> list = auntService.getAuntLocalDate(user);
+        return Result.ok(auntService.auntRecord(list));
     }
-
-    private Map<String, Object> calcAunt(List<LocalDate> list) {
-        Map<String, Object> result = new HashMap<>(8);
-        LocalDate first = list.get(0);
-        LocalDate second = list.get(1);
-
-        long min = first.until(second, ChronoUnit.DAYS);
-        long max = min;
-        long sum = 0L;
-        // 姨妈间隔时间
-        List<String> auntInterval = new ArrayList<>(list.size() - 1);
-        // 姨妈出现次数
-        TreeMap<Long, Long> countWithDays = new TreeMap<>();
-
-        for (int i = 0; i < list.size() - 1; i++) {
-            LocalDate before = list.get(i);
-            LocalDate after = list.get(i + 1);
-            long until = before.until(after, ChronoUnit.DAYS);
-            Long value = countWithDays.get(until);
-            if (value == null) {
-                countWithDays.put(until, 1L);
-            } else {
-                countWithDays.put(until, value + 1);
-            }
-
-            if (until < min) {
-                min = until;
-            }
-            if (until > max) {
-                max = until;
-            }
-            sum += until;
-            auntInterval.add(before.getYear() + "年" + before.getMonthValue() + "月到" + after.getMonthValue() + "月相差" + until + "天");
-        }
-
-        AtomicLong maxCount = new AtomicLong(0L);
-        // 姨妈出现次数
-        List<String> daysInterval = new ArrayList<>(countWithDays.size());
-        countWithDays.forEach((day, count) -> {
-            if (count > maxCount.get()) {
-                maxCount.set(count);
-            }
-            daysInterval.add(day + "天出现" + count + "次");
-        });
-        StringBuilder daysMax = new StringBuilder();
-        countWithDays.forEach((day, count) -> {
-            if(count.equals(maxCount.get()) ){
-                daysMax.append(day).append(",");
-            }
-        });
-        daysMax.replace(daysMax.length()-1,daysMax.length(),"");
-        daysMax.append("天出现次数最多,次数为: ").append(maxCount).append("次");
-        BigDecimal avg = new BigDecimal(sum).divide(new BigDecimal(list.size() - 1), 2, RoundingMode.HALF_UP);
-        BigDecimal nextAunt = new BigDecimal(sum).divide(new BigDecimal(list.size() - 1), 0, RoundingMode.HALF_UP);
-        result.put("auntInterval", auntInterval);
-        result.put("daysInterval", daysInterval);
-        result.put("daysMax", daysMax);
-        result.put("minInterval", min);
-        result.put("maxInterval", max);
-        result.put("avgInterval", avg);
-        result.put("nextAunt", "预计下次" + list.get(list.size() - 1).plusDays(nextAunt.longValue()));
-        return result;
-    }
-
 }
--- GeeMQ/src/main/java/com/gee/mq/service/AuntService.java
+++ GeeMQ/src/main/java/com/gee/mq/service/AuntService.java
@@ -3,9 +3,20 @@ package com.gee.mq.service;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gee.mq.bean.Aunt;
+import com.gee.mq.bean.User;
 import com.gee.mq.config.GeeException;
 import com.gee.mq.mapper.AuntMapper;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Service
 public class AuntService extends ServiceImpl<AuntMapper, Aunt> {
@@ -23,5 +34,99 @@ public class AuntService extends ServiceImpl<AuntMapper, Aunt> {
 
         save(aunt);
 
+    }
+
+    public List<LocalDate> getAuntLocalDate(User user) {
+        List<Aunt> auntList = list(new LambdaQueryWrapper<Aunt>()
+                .eq(Aunt::getUserId, user.getId())
+                .orderByAsc(Aunt::getAuntDate));
+        return auntList.stream().map(item -> {
+            Date auntDate = item.getAuntDate();
+            return auntDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        }).collect(Collectors.toList());
+    }
+
+    public List<String> auntInterval(List<LocalDate> list) {
+        List<String> auntInterval = new ArrayList<>(list.size() - 1);
+        if (list.size() >= 2) {
+            for (int i = 0; i < list.size() - 1; i++) {
+                LocalDate before = list.get(i);
+                LocalDate after = list.get(i + 1);
+                long until = before.until(after, ChronoUnit.DAYS);
+
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月");
+                auntInterval.add(before.format(dateTimeFormatter) + "到" + after.format(dateTimeFormatter) + "相差" + until + "天");
+            }
+        } else {
+            auntInterval.add("此功能需保存两次及以上记录");
+        }
+
+        return auntInterval;
+    }
+
+    public Map<String, Object> auntRecord(List<LocalDate> list) {
+        LocalDate first = list.get(0);
+        LocalDate second = list.get(1);
+        TreeMap<Long, Long> countWithDays = new TreeMap<>();
+        Map<String, Object> result = new HashMap<>();
+        long min = first.until(second, ChronoUnit.DAYS);
+        long max = min;
+        long sum = 0L;
+        countWithDays.put(min, 1L);
+        if (list.size() >= 2) {
+            for (int i = 1; i < list.size() - 1; i++) {
+                LocalDate before = list.get(i);
+                LocalDate after = list.get(i + 1);
+                long until = before.until(after, ChronoUnit.DAYS);
+                Long value = countWithDays.get(until);
+                if (value == null) {
+                    countWithDays.put(until, 1L);
+                } else {
+                    countWithDays.put(until, value + 1);
+                }
+
+                if (until < min) {
+                    min = until;
+                }
+
+                if (until > max) {
+                    max = until;
+                }
+                sum += until;
+            }
+
+            AtomicLong maxCount = new AtomicLong(0L);
+            StringBuilder daysMax = new StringBuilder();
+            List<String> dayCount = new ArrayList<>();
+            countWithDays.forEach((day, count) -> {
+                if (count > maxCount.get()) {
+                    maxCount.set(count);
+                }
+                dayCount.add(day + "天出现" + count + "次");
+            });
+
+            countWithDays.forEach((day, count) -> {
+                if (count.equals(maxCount.get())) {
+                    daysMax.append(day).append("天,");
+                }
+            });
+
+            daysMax.replace(daysMax.length() - 1, daysMax.length(), "")
+                    .append("出现次数最多，次数为").append(maxCount).append("次");
+
+            BigDecimal avg = new BigDecimal(sum).divide(new BigDecimal(list.size() - 1), 2, RoundingMode.HALF_UP);
+            LocalDate nextDay = list.get(list.size() - 1).plusDays(
+                    new BigDecimal(sum).divide(new BigDecimal(list.size() - 1), 0, RoundingMode.HALF_UP).longValue());
+            result.put("min", "最小间隔: " + min + "天");
+            result.put("max", "最大间隔: " + max + "天");
+            result.put("avg", "平均间隔: " + avg + "天");
+            result.put("daysMax", daysMax);
+            result.put("dayCount", dayCount);
+            result.put("nextDays", "预计下次时间为" + nextDay.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日")));
+        } else {
+
+            result.put("message", "此功能需保存两次及以上记录");
+        }
+        return result;
     }
 }
