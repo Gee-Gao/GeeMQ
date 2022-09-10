f477471c7b35d19946479902ad6bb31ddac67075
Gee <1176243316@qq.com>
2022-09-10 11:35:50 +0800

    1.最近12个月天数趋势图 2.代码细节优化

--- GeeMQ/src/main/java/com/gee/mq/bean/EchartsData.java
+++ GeeMQ/src/main/java/com/gee/mq/bean/EchartsData.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PercentData {
+public class EchartsData {
     private String name;
     private BigDecimal value;
 }
--- GeeMQ/src/main/java/com/gee/mq/service/AuntService.java
+++ GeeMQ/src/main/java/com/gee/mq/service/AuntService.java
@@ -3,7 +3,7 @@ package com.gee.mq.service;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.gee.mq.bean.Aunt;
-import com.gee.mq.bean.PercentData;
+import com.gee.mq.bean.EchartsData;
 import com.gee.mq.bean.User;
 import com.gee.mq.config.GeeException;
 import com.gee.mq.mapper.AuntMapper;
@@ -76,9 +76,7 @@ public class AuntService extends ServiceImpl<AuntMapper, Aunt> {
 
         if (list.size() >= 2) {
             for (int i = 1; i < list.size() - 1; i++) {
-                LocalDate before = list.get(i);
-                LocalDate after = list.get(i + 1);
-                long until = before.until(after, ChronoUnit.DAYS);
+                long until = getDaySubtract(list, i);
                 if (until < min) {
                     min = until;
                 }
@@ -126,31 +124,52 @@ public class AuntService extends ServiceImpl<AuntMapper, Aunt> {
         return result;
     }
 
-    public List<PercentData> echarts(List<LocalDate> list) {
-        List<PercentData> result = new ArrayList<>();
+    public Map<String, Object> echarts(List<LocalDate> list) {
+        HashMap<String, Object> result = new HashMap<>();
+
+        // 天数占比
+        List<EchartsData> daysPercentList = new ArrayList<>();
         TreeMap<Long, Long> countWithDays = getCountWithDays(list);
         BigDecimal hundred = new BigDecimal(100);
         countWithDays.forEach((day, count) -> {
-            BigDecimal percent = new BigDecimal(count).divide(new BigDecimal(countWithDays.size()), 4, RoundingMode.HALF_UP).multiply(hundred);
-            PercentData percentData = new PercentData(day + "天", percent);
-            result.add(percentData);
+            BigDecimal percent = new BigDecimal(count).divide(new BigDecimal(list.size() - 1), 4, RoundingMode.HALF_UP).multiply(hundred);
+            EchartsData echartsData = new EchartsData(day + "天", percent);
+            daysPercentList.add(echartsData);
         });
         BigDecimal lastPercent = new BigDecimal(100);
-        for (int i = 0; i < result.size() - 1; i++) {
-            BigDecimal percent = result.get(i).getValue();
+        for (int i = 0; i < daysPercentList.size() - 1; i++) {
+            BigDecimal percent = daysPercentList.get(i).getValue();
             lastPercent = lastPercent.subtract(percent);
         }
-        PercentData percentData = result.get(result.size() - 1);
-        percentData.setValue(lastPercent);
+        EchartsData echartsDataLast = daysPercentList.get(daysPercentList.size() - 1);
+        echartsDataLast.setValue(lastPercent);
+        result.put("daysPercentList", daysPercentList);
+
+        // 天数趋势
+        List<EchartsData> daysTrendList = new ArrayList<>();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月");
+        if (list.size() > 8) {
+            for (int i = list.size() - 8; i < list.size() - 1; i++) {
+                long daySubtract = getDaySubtract(list, i);
+                EchartsData echartsData = new EchartsData(list.get(i).format(dateTimeFormatter), new BigDecimal(daySubtract));
+                daysTrendList.add(echartsData);
+            }
+        } else {
+            for (int i = 0; i < list.size() - 1; i++) {
+                long daySubtract = getDaySubtract(list, i);
+                EchartsData echartsData = new EchartsData(list.get(i).format(dateTimeFormatter), new BigDecimal(daySubtract));
+                daysTrendList.add(echartsData);
+            }
+        }
+
+        result.put("daysTrendList", daysTrendList);
         return result;
     }
 
     public TreeMap<Long, Long> getCountWithDays(List<LocalDate> list) {
         TreeMap<Long, Long> countWithDays = new TreeMap<>();
         for (int i = 0; i < list.size() - 1; i++) {
-            LocalDate before = list.get(i);
-            LocalDate after = list.get(i + 1);
-            long until = before.until(after, ChronoUnit.DAYS);
+            long until = getDaySubtract(list, i);
             Long value = countWithDays.get(until);
             if (value == null) {
                 countWithDays.put(until, 1L);
@@ -159,5 +178,11 @@ public class AuntService extends ServiceImpl<AuntMapper, Aunt> {
             }
         }
         return countWithDays;
+    }
+
+    private long getDaySubtract(List<LocalDate> list, int i) {
+        LocalDate before = list.get(i);
+        LocalDate after = list.get(i + 1);
+        return before.until(after, ChronoUnit.DAYS);
     }
 }
